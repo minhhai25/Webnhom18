29ac3f6172a81f9614d0dcd46f2467ccf2961d7f
minhhai25 <ntmhai25@gmail.com>
2023-10-25 14:39:09 +0700

    update-v1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import router from "../src/router/index";
+import store from "./store/store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  store,
+  router,
+}).$mount("#app");
