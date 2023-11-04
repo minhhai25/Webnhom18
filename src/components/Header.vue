df4406c55d56d813b46b712da4a8b82c72dd3e8c
haphuong <phamphuongpxa@gmail.com>
2023-11-04 21:40:03 +0700

    9h39pm

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="header">
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6">
+      <div>Monday</div>
+      <img src="https://s.yimg.com/cv/apiv2/aolfp/images/weather/small/29-partly-cloudy-night.svg">
+      <span>85C</span>
+      <span class="kk">
+        <span class="icon">
+          <path d="M4.57862 0.733744C4.37805 0.924318 0 5.4431 0 9C0 11.5764 2.05916 13.6667 5 13.6667C7.94055 13.6667 10 11.5788 10 9C10 5.55182 5.61738 0.932934 5.41653 0.738052L5 0.333336L4.57862 0.733744ZM5.23851 10.5159C4.92326 10.5212 4.67276 10.7523 4.67876 11.0323C4.68475 11.3091 4.93895 11.5296 5.24906 11.5296H5.26019C6.78339 11.5035 8.02272 10.3821 8.02272 9.02929C8.02272 8.58808 7.89119 8.15447 7.64242 7.7751C7.48351 7.53333 7.13402 7.45198 6.86185 7.59238C6.58967 7.73328 6.49752 8.04372 6.65615 8.28549C6.80336 8.51028 6.88153 8.76725 6.88153 9.02929C6.88153 9.8334 6.14432 10.5004 5.23851 10.5159Z"></path>
+        </span>
+        <span>6%</span>
+    </div>
+    <div class="col-lg-6">
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+    </div>
+  </div>
+</div>
    
   </div>
 </template>
