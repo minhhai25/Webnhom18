d79b5aae24b353b09eb009f029e16b2bc4d3d453
hue28 <27112002danghue@gmail.com>
2023-11-04 19:24:48 +0700

    hoan thanh html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <comp-home></comp-home>
+    <!-- <weather-page></weather-page> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CompHome from './components/CompHome.vue';
+// import WeatherPage from './components/Weather.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+//import CompHome from './components/CompHome.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    // WeatherPage,
+    CompHome,
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
