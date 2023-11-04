b70bd675f03302ee288a8e52213ee899aa4286a5
minhhai25 <ntmhai25@gmail.com>
2023-11-04 21:31:35 +0700

    up-9h31

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,18 @@
 <template>
-  <div class="header">
-   
-  </div>
+  <div class="header"></div>
 </template>
 
 <script>
 export default {
-  name:"header-comp",
+  name: "header-comp",
   components: {},
 
   data() {
-    return {
-    
-    };
+    return {};
   },
 
-  methods: {
- 
-  },
+  methods: {},
 };
 </script>
 
-<style>
-
-
-</style>
+<style></style>
