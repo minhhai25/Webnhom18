0ec20d5413ba54fe275e28d9da627754a2aa614b
minhhai25 <ntmhai25@gmail.com>
2023-11-05 09:55:54 +0700

    9h55 11-5

--- src/components/CompHome.vue
+++ src/components/CompHome.vue
@@ -2,15 +2,18 @@
   <div class = " home">
     <h1>Weather</h1>
     <weather-page></weather-page>
+    <header-comp></header-comp>
   </div>
 </template>
 <script>
 import WeatherPage from './Weather.vue';
+import Headers from "./Header.vue";
 
 export default {
  components: {
     // HelloWorld
     WeatherPage,
+     "header-comp": Headers,
   },
   name
 
