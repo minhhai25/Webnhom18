7b01a22c3b65cdc949d80649f488debb9ad33595
haphuong <phamphuongpxa@gmail.com>
2023-11-05 15:12:29 +0700

    3h12pm

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,69 +1,162 @@
 <template>
   <div class="header">
+    
     <div class="container">
-      <div class="row">
-        <div class="col-md 2">
+      <div class="forecast-button">
+       <button type="button" class="hour-button btn btn-primary active">Hourly</button> 
+       <button type="button" class="date-button btn btn-primary disabled">10 Days</button>     
+    </div>
+    <!-- Theo thu -->
+      <div class="row weather-days">
+        <div class="col-md-2">
           <span class="date">Monday</span>
-          <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <span class="mua">
+             <img src="../assets/images/mua.png" alt=""/>
+            </span>
             <span class="rain">15%</span>
           </span>
         </div>
-       <div class="col-md 2">
+       <div class="col-md-2">
           <span class="date">Tuesday</span>
-          <img src="" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <img src="../assets/images/mua.png" alt=""/>
             <span class="rain">15%</span>
           </span>
         </div>
-        <div class="col-md 2">
+        <div class="col-md-2">
           <span class="date">Thursday</span>
-          <img src="" alt=""/>
+        <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <img src="../assets/images/mua.png" alt=""/>
             <span class="rain">15%</span>
           </span>
         </div>
-        <div class="col-md 2">
+        <div class="col-md-2">
           <span class="date">Friday</span>
-          <img src="" alt=""/>
+         <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <img src="../assets/images/mua.png" alt=""/>
             <span class="rain">15%</span>
           </span>
         </div>
-        <div class="col-md 2">
-          <span class="date"></span>
-          <img src="" alt=""/>
+        <div class="col-md-2">
+          <span class="date">Saturday</span>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <img src="../assets/images/mua.png" alt=""/>
             <span class="rain">15%</span>
           </span>
         </div>
-        <div class="col-md 2">
+        <div class="col-md-2">
           <span class="date">Sunday</span>
-          <img src="" alt=""/>
+         <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="" alt=""/>
+            <img src="../assets/images/mua.png" alt=""/>
+            <span class="rain">15%</span>
+          </span>
+        </div> 
+      </div>
+      <hr>
+      <!-- Theo gio -->
+      <div class="row weather-hour">
+        <div class="col-md-2">
+          <span class="date">2PM</span>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <span class="mua">
+             <img src="../assets/images/mua.png" alt=""/>
+            </span>
+            <span class="rain">15%</span>
+          </span>
+        </div>
+       <div class="col-md-2">
+          <span class="date">3PM</span>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <img src="../assets/images/mua.png" alt=""/>
+            <span class="rain">15%</span>
+          </span>
+        </div>
+        <div class="col-md-2">
+          <span class="date">4PM</span>
+        <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <img src="../assets/images/mua.png" alt=""/>
+            <span class="rain">15%</span>
+          </span>
+        </div>
+        <div class="col-md-2">
+          <span class="date">5PM</span>
+         <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <img src="../assets/images/mua.png" alt=""/>
+            <span class="rain">15%</span>
+          </span>
+        </div>
+        <div class="col-md-2">
+          <span class="date">6PM</span>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <img src="../assets/images/mua.png" alt=""/>
+            <span class="rain">15%</span>
+          </span>
+        </div>
+        <div class="col-md-2">
+          <span class="date">7PM</span>
+         <span class="anh">
+            <img src="../assets/images/img01.png" alt=""/>
+          </span>
+          <span class="temp">85°C</span>
+          <span class="change">
+            <img src="../assets/images/mua.png" alt=""/>
             <span class="rain">15%</span>
           </span>
         </div> 
       </div>
     </div>
-
+    
+</div>
    
-  </div>
 </template>
 
 <script>
+import '../assets/css/Forecast.css'
 export default {
   name:"forecast-comp",
   components: {},
