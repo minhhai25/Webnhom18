844b71570f0ec9bb406ac563f98a16ac141d679d
MaiHoa <lymahoa28@gmail.com>
2023-11-05 14:53:20 +0700

    css header

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,86 +1,78 @@
 <template>
-  <div class="header">
-    <div class="container">
-      <div class="row">
-        <div class="col-md 2">
-          <span class="date">Monday</span>
-          <img src="../assets/images/img01.png" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div>
-       <div class="col-md 2">
-          <span class="date">Tuesday</span>
-          <img src="" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div>
-        <div class="col-md 2">
-          <span class="date">Thursday</span>
-          <img src="" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div>
-        <div class="col-md 2">
-          <span class="date">Friday</span>
-          <img src="" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div>
-        <div class="col-md 2">
-          <span class="date"></span>
-          <img src="" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div>
-        <div class="col-md 2">
-          <span class="date">Sunday</span>
-          <img src="" alt=""/>
-          <span class="temp">85°C</span>
-          <span class="change">
-            <img src="" alt=""/>
-            <span class="rain">15%</span>
-          </span>
-        </div> 
+  <div id="header">
+    <!-- <div class="container"> -->
+    <div class="row">
+      <div class="col-md 2">
+        <span class="date">Monday</span>
+        <img src="../assets/images/img01.png" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
+      </div>
+      <div class="col-md 2">
+        <span class="date">Tuesday</span>
+        <img src="" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
+      </div>
+      <div class="col-md 2">
+        <span class="date">Thursday</span>
+        <img src="" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
+      </div>
+      <div class="col-md 2">
+        <span class="date">Friday</span>
+        <img src="" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
+      </div>
+      <div class="col-md 2">
+        <span class="date"></span>
+        <img src="" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
+      </div>
+      <div class="col-md 2">
+        <span class="date">Sunday</span>
+        <img src="" alt="" />
+        <span class="temp">85°C</span>
+        <span class="change">
+          <img src="" alt="" />
+          <span class="rain">15%</span>
+        </span>
       </div>
     </div>
-
-   
   </div>
+
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
-  name:"forecast-comp",
+  name: "forecast-comp",
   components: {},
 
   data() {
-    return {
-    
-    };
+    return {};
   },
 
-  methods: {
- 
-  },
+  methods: {},
 };
 </script>
 
-<style>
-
-
-</style>
+<style></style>
