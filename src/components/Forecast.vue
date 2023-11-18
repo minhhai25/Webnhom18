f482130e848028bb550a4cb3e2af6690c0547d0b
MaiHoa <lymahoa28@gmail.com>
2023-11-18 18:49:14 +0700

    thanh tim kiem

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,179 +1,173 @@
 <template>
-  <div class="header">
-    
-    <div class="container">
+  <div class="forecast">
+    <div class="">
       <div class="forecast-button">
-       <button type="button" class="hour-button btn btn-primary active">Hourly</button> 
-       <button type="button" class="date-button btn btn-primary disabled">10 Days</button>     
-    </div>
-    <!-- Theo thu -->
+        <button type="button" class="hour-button btn btn-primary">
+          Hourly
+        </button>
+        <button type="button" class="date-button btn btn-primary">
+          10 Days
+        </button>
+      </div>
+      <!-- Theo thu -->
       <div class="row weather-days">
         <div class="col-md-2">
           <span class="date">Monday</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
             <span class="mua">
-             <img src="../assets/images/mua.png" alt=""/>
+              <img src="../assets/images/mua.png" alt="" />
             </span>
             <span class="rain">15%</span>
           </span>
         </div>
-       <div class="col-md-2">
+        <div class="col-md-2">
           <span class="date">Tuesday</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">Thursday</span>
-        <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">Friday</span>
-         <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">Saturday</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">Sunday</span>
-         <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
-        </div> 
+        </div>
       </div>
-      <hr>
+      <hr />
       <!-- Theo gio -->
       <div class="row weather-hour">
         <div class="col-md-2">
           <span class="date">2PM</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
             <span class="mua">
-             <img src="../assets/images/mua.png" alt=""/>
+              <img src="../assets/images/mua.png" alt="" />
             </span>
             <span class="rain">15%</span>
           </span>
         </div>
-       <div class="col-md-2">
+        <div class="col-md-2">
           <span class="date">3PM</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">4PM</span>
-        <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">5PM</span>
-         <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">6PM</span>
           <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
         </div>
         <div class="col-md-2">
           <span class="date">7PM</span>
-         <span class="anh">
-            <img src="../assets/images/img01.png" alt=""/>
+          <span class="anh">
+            <img src="../assets/images/img01.png" alt="" />
           </span>
           <span class="temp">85°C</span>
           <span class="change">
-            <img src="../assets/images/mua.png" alt=""/>
+            <img src="../assets/images/mua.png" alt="" />
             <span class="rain">15%</span>
           </span>
-        </div> 
+        </div>
       </div>
     </div>
-    
-</div>
-   
+  </div>
 </template>
 
 <script>
-import '../assets/css/Forecast.css'
+import "../assets/css/Forecast.css";
 export default {
-  name:"forecast-comp",
+  name: "forecast-comp",
   components: {},
 
   data() {
-    return {
-    
-    };
+    return {};
   },
 
-  methods: {
- 
-  },
+  methods: {},
 };
 </script>
 
-<style>
-
-
-</style>
+<style></style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,74 +1,69 @@
 <template>
   <div id="test">
-    <div class="container">
-      <div class="weather-details">
-        <div class="weather-details__basic">
-          <div class="weather-details__current">
-            <img
-              src="../assets/images/1.png"
-              alt="Mostly Clear"
-              class="weather-icon"
-            />
-            <div class="weather-details__current__desc">Mostly Clear</div>
-          </div>
-          <div class="weather-details__today">
-            <div class="weather-details__today__temp">
-              <span>
-                <span class="weather__unit--metric" aria-label="27° celsius">
-                  <span aria-hidden="true">27°</span>
-                </span>
+    <!-- <div class="container"> -->
+    <div class="weather-details">
+      <div class="weather-details__basic">
+        <div class="weather-details__current">
+          <img
+            src="../assets/images/1.png"
+            alt="Mostly Clear"
+            class="weather-icon"
+          />
+          <div class="weather-details__current__desc">Mostly Clear</div>
+        </div>
+        <div class="weather-details__today">
+          <div class="weather-details__today__temp">
+            <span>
+              <span class="weather__unit--metric" aria-label="27° celsius">
+                <span aria-hidden="true">27°</span>
               </span>
-            </div>
+            </span>
+          </div>
 
-            <div class="row">
-              <div class="weather-details__today__details">
-                <div class="cod-md-4">
-                  <img src="../assets/images/upload.png" alt="" />
+          <div class="row">
+            <div class="weather-details__today__details">
+              <div class="cod-md-4">
+                <img src="../assets/images/upload.png" alt="" />
 
-                  <span>
-                    <span
-                      class="weather__unit--metric"
-                      aria-label="31° celsius"
-                    >
-                      <span aria-hidden="true">31°</span>
-                    </span>
+                <span>
+                  <span class="weather__unit--metric" aria-label="31° celsius">
+                    <span aria-hidden="true">31°</span>
                   </span>
-                </div>
-                <div class="cod-md-4">
-                  <img src="../assets/images/dowload.png" alt="" />
-                  <span>
-                    <span class="weather__unit--metric" aria-label="24° celsius"
-                      ><span aria-hidden="true">24°</span></span
-                    >
-                  </span>
-                </div>
-                <div class="cod-md-4">
-                  <img src="../assets/images/nuoc.png" alt="" />
-                  <span>
-                    <span class="weather__unit--metric" aria-label="humidity"
-                      ><span aria-hidden="true">54%</span></span
-                    >
-                  </span>
-                </div>
+                </span>
+              </div>
+              <div class="cod-md-4">
+                <img src="../assets/images/dowload.png" alt="" />
+                <span>
+                  <span class="weather__unit--metric" aria-label="24° celsius"
+                    ><span aria-hidden="true">24°</span></span
+                  >
+                </span>
+              </div>
+              <div class="cod-md-4">
+                <img src="../assets/images/nuoc.png" alt="" class="img-nuoc" />
+                <span>
+                  <span class="weather__unit--metric" aria-label="humidity"
+                    ><span aria-hidden="true">54%</span></span
+                  >
+                </span>
               </div>
             </div>
           </div>
         </div>
-        <div class="weather-details__additional-info">
-          <div class="flex-col">6:00AM<span class="label">Sunrise</span></div>
-          <div class="flex-col">83%<span class="label">Humidity</span></div>
-          <div class="flex-col">
-            29.86 in<span class="label">Pressure</span>
-          </div>
-          <div class="flex-col">--<span class="label">UV Index</span></div>
-          <div class="flex-col">6:00AM<span class="label">Sunrise</span></div>
-          <div class="flex-col">5km/h<span class="label">Wind</span></div>
-          <div class="flex-col">24°<span class="label">Dew Point</span></div>
-          <div class="flex-col">10 mi<span class="label">Visibility</span></div>
-        </div>
+      </div>
+      <div class="weather-details__additional-info">
+        <div class="flex-col">6:00AM<span class="label">Sunrise</span></div>
+        <div class="flex-col">83%<span class="label">Humidity</span></div>
+        <div class="flex-col">29.86 in<span class="label">Pressure</span></div>
+        <div class="flex-col">--<span class="label">UV Index</span></div>
+        <div class="flex-col">6:00AM<span class="label">Sunrise</span></div>
+        <div class="flex-col">5km/h<span class="label">Wind</span></div>
+        <div class="flex-col">24°<span class="label">Dew Point</span></div>
+        <div class="flex-col">10 mi<span class="label">Visibility</span></div>
       </div>
     </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <script>
